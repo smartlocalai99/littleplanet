3ebedaacef53bc1aea956eee6df26b7709c6e844
Vardhan reddy <vardhanreddy@Vardhans-MacBook-Air.local>
2026-06-11 02:34:32 +0530

    sd

--- pages/_document.js
+++ pages/_document.js
@@ -8,10 +8,12 @@ export default function Document() {
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-title" content="Little Planet" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta name="theme-color" content="#ffffff" />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="icon" href="/logo.png" type="image/png" />
-        <link rel="apple-touch-icon" href="/logo.png" />
+        <link rel="icon" href="/logo.png" type="image/png" sizes="1254x1254" />
+        <link rel="shortcut icon" href="/logo.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/logo.png" sizes="1254x1254" />
       </Head>
       <body className="antialiased">
         <Main />
